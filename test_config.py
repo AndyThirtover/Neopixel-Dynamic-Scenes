14603fd85cd9426ef29e324921de97e5d7d9673c
AndyThirtover <andy@thirtover.com>
2017-06-16 21:01:09 +0000

    testing a config idea

---
+++ test_config.py
@@ -0,0 +1,13 @@
+import yaml
+
+cfile = open('configs.yaml','w')
+cdata = {'guff':'test', 'guff2':4761}
+cfile.write(yaml.dump(cdata))
+cfile.close()
+
+
+with open('configs.yaml','r') as rfile:
+    cfg = yaml.load(rfile)
+
+for section in cfg:
+    print(section)
